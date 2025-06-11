56c3d28945dbbcd0c51ed2f7565329bab40b771c
arjav0703 <arjavjain0703@gmail.com>
2025-06-11 21:19:18 +0530

    change speed of logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ type LogoProps = {
 export default function Logo({ size }: LogoProps) {
   return (
     <div>
-      <Image src="logo.svg" alt="Logo" width={size} height={size} className="animate-spin hover:[animation-play-state:paused]" />
+      <Image src="logo.svg" alt="Logo" width={size} height={size} className="animate-spin [animation-duration:2s] hover:[animation-play-state:paused]" />
     </div>
   );
 }
