0fa6ef76bddf3620f6c27d5f5e0bbd7f4869cdbb
arjav0703 <arjavjain0703@gmail.com>
2025-06-18 20:19:23 +0530

    add padding for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <>
       <main
-        className="min-h-screen flex flex-col items-center justify-center bg-[#0a192f] relative"
+        className="min-h-screen flex flex-col items-center justify-center bg-[#0a192f] relative md:p-0 p-10"
         style={{
           fontFamily: "var(--font-sans), Arial, Helvetica, sans-serif",
         }}
       >
         <Hero />
-        <p className="absolute bottom-6 text-xl text-[#61DBFB]">Reactive is a program by <a href="https://haclclub.com" className="hover:underline">Hackclub</a>. Ends 2nd July.</p>
+        <p className="absolute bottom-6 text-xl text-[#61DBFB] text-center">Reactive is a program by <a href="https://haclclub.com" className="hover:underline">Hackclub</a>. Ends 2nd July.</p>
       </main>
 
       <Details/>
