48fd2891d84c4bc1e987a947a56e0b8214e7be09
Arjav Jain <arjavjain0703@gmail.com>
2025-10-01 23:47:57 +0530

    update link

--- components/Details.tsx
+++ components/Details.tsx
@@ -50,7 +50,7 @@ function Requirements() {
           </ul>
           <div className="flex justify-end gap-3">
             <Button href="https://www.figma.com/slides/Gqg9is3DM3BFmlUB6yTArH/Reactive-Guide?node-id=1-42&t=0ELDHDCDVg1XLXZX-1">Guide</Button>
-            <Button href="https://forms.hackclub.com/t/fRtCcb5PZUus">Submit</Button>
+            <Button href="https://forms.hackclub.com/reactive-submissions">Submit</Button>
           </div>
         </div>
       </section>
